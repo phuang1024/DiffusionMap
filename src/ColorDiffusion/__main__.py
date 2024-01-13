1997ab654131e37a78a04b385c6ac871e10e6c8f
Patrick Huang <huangpatrick16777216@gmail.com>
2024-01-13 11:42:41 -0800

    Changed arg names.

--- src/ColorDiffusion/__main__.py
+++ src/ColorDiffusion/__main__.py
@@ -9,10 +9,10 @@ def main():
     parser.add_argument("data", type=Path)
     parser.add_argument("results", type=Path)
     parser.add_argument("--epochs", type=int, default=100)
-    parser.add_argument("--train-mult", type=int, default=5, help="Train for N epochs per epoch")
+    parser.add_argument("--train_mult", type=int, default=5, help="Train for N epochs per epoch")
     parser.add_argument("--res", type=int, default=256)
     parser.add_argument("--unet_dim", type=int, default=64)
-    parser.add_argument("--diffusion-steps", type=int, default=100)
+    parser.add_argument("--diffusion_steps", type=int, default=100)
     args = parser.parse_args()
 
     args.results.mkdir(exist_ok=True)
