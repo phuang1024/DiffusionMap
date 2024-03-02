adad290c350085ac2e0e2e77cadc0347756fc31d
Patrick Huang <huangpatrick16777216@gmail.com>
2024-03-02 14:45:36 -0800

    Bump version to 0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'DiffusionMap'
 copyright = '2024, Patrick Huang'
 author = 'Patrick Huang'
-release = '0.1.1'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
